75db2a1ef8c8942c80236a8f1d99df9e815325eb
milochadam <miloch.adam@gmail.com>
2022-10-22 08:59:23 +0200

    display some image

--- main.cpp
+++ main.cpp
@@ -10,41 +10,103 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 
-int main() {
-    std::cout << "Hello. Initialising SDL2." << std::endl;
+class Surface {
+public:
+    SDL_Surface* surface;
+public:
+    Surface(std::string path) {
+        this->surface = SDL_LoadBMP(path.c_str());
+    }
+    ~Surface() { SDL_FreeSurface(this->surface); }
+};
 
-    SDL_Window* window = nullptr;
-    SDL_Surface* screen_surface = nullptr;
 
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+class Game {
+
+    SDL_Window *window = nullptr;
+    SDL_Surface *screen_surface = nullptr;
+
+public:
+    Game() {}
+
+    ~Game() {
         SDL_DestroyWindow(window);
         SDL_Quit();
-        return 1;
+    }
+    int init() {
+        std::cout << "Hello. Initialising SDL2." << std::endl;
+
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+            std::cout << "SDL could not initialise! SDL_Error: " << SDL_GetError()
+                      << std::endl;
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 1;
+        }
+        this->window = SDL_CreateWindow("edo", SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                        SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (!window) {
+            std::cout << "Window could not be created! SDL_Error: "
+                      << SDL_GetError() << std::endl;
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 1;
+        }
+        return 0;
     }
 
-    window = SDL_CreateWindow("edo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if(!window) {
-        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError()
-                  << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
 
-    screen_surface = SDL_GetWindowSurface(window);
-    SDL_FillRect(screen_surface, nullptr, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
-    SDL_UpdateWindowSurface(window);
-    SDL_Event e;
-    bool quit = false;
-    while(!quit) {
-        while(SDL_PollEvent(&e)) {
-            if(e.type == SDL_QUIT) {
-                quit = true;
+    void start() {
+
+        Surface placeholder_img = Surface("placeholder.bmp");
+
+        screen_surface = SDL_GetWindowSurface(window);
+        SDL_FillRect(screen_surface, nullptr,
+                     SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+
+        SDL_Event e;
+        bool quit = false;
+        while (!quit) {
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                } else if (e.type == SDL_KEYDOWN) {
+                    switch (e.key.keysym.sym) {
+                    case SDLK_UP:
+                        break;
+                    case SDLK_DOWN:
+                        break;
+                    case SDLK_LEFT:
+                        break;
+                    case SDLK_RIGHT:
+                        break;
+                    case SDLK_q:
+                        quit = true;
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                SDL_Rect rect;
+                rect.x = 0;
+                rect.y = 0;
+                rect.w = SCREEN_WIDTH / 2;
+                rect.h = SCREEN_HEIGHT / 2;
+
+                SDL_BlitSurface(placeholder_img.surface, NULL, screen_surface, &rect);
+                SDL_UpdateWindowSurface(window);
             }
         }
     }
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+};
+
+int main() {
+    Game game = Game();
+    if (game.init()) {
+        return 1;
+    }
+    game.start();
     return 0;
+
 }
